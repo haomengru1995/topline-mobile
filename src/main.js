8bfd8b015d467c715c2036908abf612335423e05
hao <2595004299@qq.com>
2019-07-09 19:13:43 +0800

    登录-页面布局

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import store from './store'
 // 这个第三包引入即可，它会自动根据页面宽度设置网页基准字体大小
 import 'amfe-flexible'
 import 'postcss-pxtorem'
+
 import {
+  NavBar,
+  Button,
+  Field
+
 } from 'vant'
+Vue.use(NavBar).use(Button).use(Field)
 
 Vue.config.productionTip = false
 
