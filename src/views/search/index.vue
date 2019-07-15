6e15ad69cd1ea24c2db7fc11177640a5d891a147
hao <2595004299@qq.com>
2019-07-15 15:18:15 +0800

    搜索-联想建议-基本完成

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -7,14 +7,12 @@
     />
     <!-- 联想建议列表 -->
     <van-cell-group>
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
+      <van-cell
+        icon="search"
+        v-for="item in suggestions"
+        :key="item"
+        :title="item"
+      />
     </van-cell-group>
     <!-- /联想建议列表 -->
 
@@ -33,14 +31,27 @@
 </template>
 
 <script>
+import { getSuggestion } from '@/api/search'
 export default {
   name: 'SearchIndex',
   data () {
     return {
-      searchText: ''
+      searchText: '',
+      suggestions: []
+    }
+  },
+  watch: {
+    async searchText (newVal) {
+      newVal = newVal.trim() // 去除首尾空格
+      // 如果数据为空，则什么都不做
+      if (!newVal) {
+        return
+      }
+      // 如果数据不为空，则请求联想建议自动补全
+      const data = await getSuggestion(newVal)
+      this.suggestions = data.options
     }
   }
 }
 </script>
-
 <style lang="less" scoped></style>
