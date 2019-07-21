f3b5dadbe4102cb77f6292d8e8e7b9f6756bc065
hao <2595004299@qq.com>
2019-07-22 02:55:58 +0800

    解决展示不同评论的回复列表更新问题

--- src/views/article/components/reply-list.vue
+++ src/views/article/components/reply-list.vue
@@ -7,7 +7,7 @@
     @close="handleClose"
     @open="handleOpen"
   >
-    <comment-list ref="comment-list" :source="commentId" :isArticle="false" />
+    <comment-list v-if="value" ref="comment-list" :source="commentId" :isArticle="false" />
     <write-comment :target="commentId" :article-id="articleId" />
   </van-popup>
 </template>
@@ -36,27 +36,7 @@ export default {
   data () {
     return {}
   },
-  created () {},
-  activated () {
-    console.log('activated')
-  },
-  deactivated () {
-    console.log('deactivated')
-  },
-  methods: {
-    handleClose () {
-      // refs 有两个作用：
-      // 1. 获取 DOM
-      // 2. 获取一个组件
-      // 最简单粗暴的方式：直接通过 ref 拿到这个组件本身，操作里面的数据得到你想要的结果
-      this.$refs['comment-list'].comments = []
-      this.$refs['comment-list'].finished = false
-      this.$refs['comment-list'].offset = null
-    },
-    handleOpen () {
-      this.$refs['comment-list'].onLoad()
-      // this.$refs['comment-list'].loading = true
-    }
+  created () {
   }
 }
 </script>
