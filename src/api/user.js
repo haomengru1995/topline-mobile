6ba5b8358831da8eaef7856910204ae01f9ca266
hao <2595004299@qq.com>
2019-07-22 16:22:56 +0800

    编辑个人信息-准备组件

--- src/api/user.js
+++ src/api/user.js
@@ -42,3 +42,12 @@ export const unFollowUser = userId => {
     url: `/app/v1_0/user/followings/${userId}`
   })
 }
+/**
+ * 获取用户自己信息
+ */
+export const getMyInfo = userId => {
+  return request({
+    method: 'GET',
+    url: `/app/v1_0/user`
+  })
+}
