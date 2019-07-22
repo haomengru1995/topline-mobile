4e56eab31b8549df2aa7bb902daf73fe274ffc2e
hao <2595004299@qq.com>
2019-07-22 19:38:49 +0800

    处理token过期问题

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -6,6 +6,23 @@
 
 <script>
 import io from 'socket.io-client'
+/**
+ * 1. 建立连接
+ * 发送与接收数据都是事件的形式（API的使用方式）
+ * 网络通信中一个核心概念术语：Socket
+ * 2. 发送数据
+ *  WebSocket WebSocket.send(data)
+ *  socket.emit('消息类型', 数据体)
+ *    消息类型说白了就好比 http 协议中的请求路径
+ *    这是 socket.io 封装之后的使用方式，目的是为了区别不同的数据
+ *    消息类型都是后台指定的，就好比接口路径一样，人家设计成什么，我们就怎么使用
+ * 3. 接收数据
+ *  socket.on('消息类型', 数据体)
+ *  这个消息类型还是由后端指定的
+ *  一种方式就是后端完全主导，他说类型是什么你就怎么用
+ *  有时候也需要前端
+ *  socket.on('notify', 数据体)
+ */
 export default {
   name: 'ChatIndex',
   data () {
